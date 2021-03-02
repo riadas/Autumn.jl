1a0f765641bdbc22415f7c4effbf6ac52bda4f23
Zenna Tavares <zennatavares@gmail.com>
2021-03-02 15:57:15 -0500

    rm build

--- docs/build/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"#Autumn.jl","page":"Home","title":"Autumn.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
-}
