3ebad17e52e14a3f9825c1dfbdb892ef2ad0edb8
Ria Das <riadas@mit.edu>
2021-03-06 11:07:37 -0500

    fixed isinferred version issue

--- test/compile.jl
+++ test/compile.jl
@@ -12,11 +12,17 @@ end
 
 mod = nothing
 
+if VERSION > v"1.6-"
+  const ts = Test.typesplit
+else
+  const ts = Test.typesubtract
+end
+
 function isinferred(f, args...; allow = Union{})
   ret = f(args...)
   inftypes = Base.return_types(f, Base.typesof(args...))
   rettype = ret isa Type ? Type{ret} : typeof(ret)
-  rettype <: allow || rettype == Test.typesubtract(inftypes[1], allow)
+  rettype <: allow || rettype == ts(inftypes[1], allow)
 end
 
 function test_compile_if()
