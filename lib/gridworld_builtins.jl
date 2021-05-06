768ef3c5b0c56d1af2d676c21aa943ddd2c2241a
Emily Fan <emilyfan8@gmail.com>
2021-05-05 20:24:46 -0400

    move object/occurred out to autumn base to gridworld

--- lib/gridworld_builtins.jl
+++ lib/gridworld_builtins.jl
@@ -1,5 +1,69 @@
 module GridworldBase
 
+abstract type Object end
+
+# const general_builtInDict = Dict([
+  # "occurred"        =>  quote
+  #                         function occurred(click)
+  #                           click !== nothing
+  #                         end
+  #                       end,
+
+function addObj(list::Array{<:Object}, obj::Object)
+  new_list = vcat(list, obj)
+  new_list
+end
+
+function addObj(list::Array{<:Object}, objs::Array{<:Object})
+  new_list = vcat(list, objs)
+  new_list
+end
+
+function removeObj(list::Array{<:Object}, obj::Object)
+  filter(x -> x.id != obj.id, list)
+end
+
+function removeObj(list::Array{<:Object}, fn)
+  orig_list = filter(obj -> !fn(obj), list)
+end
+
+function removeObj(obj::Object)
+  obj.alive = false
+  deepcopy(obj)
+end
+
+function updateObj(obj::Object, field::String, value)
+  fields = fieldnames(typeof(obj))
+  custom_fields = fields[5:end-1]
+  origin_field = (fields[2],)
+
+  constructor_fields = (custom_fields..., origin_field...)
+  constructor_values = map(x -> x == Symbol(field) ? value : getproperty(obj, x), constructor_fields)
+
+  new_obj = typeof(obj)(constructor_values...)
+  setproperty!(new_obj, :id, obj.id)
+  setproperty!(new_obj, :alive, obj.alive)
+  setproperty!(new_obj, :hidden, obj.hidden)
+
+  setproperty!(new_obj, Symbol(field), value)
+  state.objectsCreated -= 1    
+  new_obj
+end
+
+function updateObj(list::Array{<:Object}, map_fn, filter_fn=filter_fallback)
+  orig_list = filter(obj -> !filter_fn(obj), list)
+  filtered_list = filter(filter_fn, list)
+  new_filtered_list = map(map_fn, filtered_list)
+  vcat(orig_list, new_filtered_list)
+end
+
+function filter_fallback(obj::Object)
+  true
+end
+
+
+
+
 abstract type KeyPress end
 
 struct Left <: KeyPress end
