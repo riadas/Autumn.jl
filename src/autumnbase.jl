e7ba514f7d3cf2559d899da47da511ecaa8a0b57
Amber Li <amli@mit.edu>
2021-05-10 23:41:42 -0400

    fixed uniformChoice, added updateObj fncs

--- src/autumnbase.jl
+++ src/autumnbase.jl
@@ -3,13 +3,13 @@ module AutumnBase
 
 using Distributions: Categorical
 import Base.range, Base.min
-export uniformChoice, uniformChoice2, min, range
+export uniformChoice, min, range, updateObj
 
 function uniformChoice(rng, freePositions)
   freePositions[rand(rng, Categorical(ones(length(freePositions))/length(freePositions)))]
 end
 
-function uniformChoice2(rng, freePositions, n)
+function uniformChoice(rng, freePositions, n)
   map(idx -> freePositions[idx], rand(rng, Categorical(ones(length(freePositions))/length(freePositions)), n))
 end
 
@@ -20,6 +20,27 @@ end
 
 function Base.range(start::Int, stop::Int)
   Base.range(start, stop=stop)
+end
+
+function updateObj(obj, field::String, value)
+  fields = fieldnames(typeof(obj))
+  constructor_fields = fields
+  constructor_values = map(x -> x == Symbol(field) ? value : getproperty(obj, x), fields)
+
+  new_obj = typeof(obj)(constructor_values...)
+  # setproperty!(new_obj, Symbol(field), value)
+  new_obj
+end
+
+function updateObj(list::Array, map_fn, filter_fn=filter_fallback)
+  orig_list = filter(obj -> !filter_fn(obj), list)
+  filtered_list = filter(filter_fn, list)
+  new_filtered_list = map(map_fn, filtered_list)
+  vcat(orig_list, new_filtered_list)
+end
+
+function filter_fallback(obj)
+  true
 end
 
 end
