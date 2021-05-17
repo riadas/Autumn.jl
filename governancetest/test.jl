edaf044c5629fff141e76a18253201b0ae76ea93
Emily Fan <emilyfan8@gmail.com>
2021-05-17 00:18:38 -0400

    Add test for nested include

--- governancetest/test.jl
+++ governancetest/test.jl
@@ -1,6 +1,29 @@
 println(pwd())
 using Autumn
 
+io = open("/Users/work/Desktop/UROP/Autumn.jl/lib/NOTAModule.au", "r")
+# io = open("/Users/work/Desktop/UROP/Governance.jl/autumn/sortition_includes.lisp", "r")
+prog_autumn = read(io, String)  # this
+prog_autumn = parseautumn(prog_autumn)
+prog_julia = compiletojulia(prog_autumn)
+open("/Users/work/Desktop/UROP/Autumn.jl/governancetest/CompiledTestImport.jl", "w") do io1
+   # write(io1, string(prog_julia))
+   write(io1, string(prog_julia.args[1]))
+end
+
+# open("../lib/NOTAModule.au", "r") do io
+#    prog_autumn = read(io, String)  # this
+#    # println(prog_autumn)
+
+#    # println(apple)
+#    println(compiletojulia)
+#    # prog_julia = compiletojulia(prog_autumn)
+#    # open("governancetest/CompiledAutumnCode2.jl", "w") do io1
+#    #    write(io1, string(prog_julia))
+#    # end
+# end
+
+
 # println("I like pi")
 # prog_autumn = au"""(program
 # (= GRID_SIZE 16)
@@ -34,24 +57,3 @@ using Autumn
 # )"""
 
 # prog_autumn = Nothing
-
-io = open("lib/stdlib.au", "r")
-prog_autumn = read(io, String)  # this
-prog_autumn = parseautumn(prog_autumn)
-prog_julia = compiletojulia(prog_autumn)
-open("governancetest/CompiledAutumnCode2.jl", "w") do io1
-   # write(io1, string(prog_julia))
-   write(io1, string(prog_julia.args[1]))
-end
-
-# open("lib/stdlib.au", "r") do io
-#    prog_autumn = read(io, String)  # this
-#    println(prog_autumn)
-
-#    # println(apple)
-#    println(compiletojulia)
-#    # prog_julia = compiletojulia(prog_autumn)
-#    # open("governancetest/CompiledAutumnCode2.jl", "w") do io1
-#    #    write(io1, string(prog_julia))
-#    # end
-# end
