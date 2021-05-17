304ff4996f0dc25000751c4083772011de4929c6
Emily Fan <emilyfan8@gmail.com>
2021-05-17 00:22:55 -0400

    Comment out sub_import and sub_include

--- src/compile.jl
+++ src/compile.jl
@@ -9,25 +9,25 @@ export compiletojulia, runprogram, compiletosketch, AULIBPATH
 
 const AULIBPATH = joinpath(dirname(pathof(Autumn)), "..", "lib")
 
-# Return an AExpr where all includes have been replaced with the code that it points to
-function sub_includes(aexpr::AExpr)
-  # this is a short-circuit or, so then it would only compute error when the first statement is false
-  aexpr.head == :program || error("Expects program Aexpr")
+# # Return an AExpr where all includes have been replaced with the code that it points to
+# function sub_includes(aexpr::AExpr)
+#   # this is a short-circuit or, so then it would only compute error when the first statement is false
+#   aexpr.head == :program || error("Expects program Aexpr")
 
-  # copy and paste the include source file into our actual autumn file
-  newargs = []
-  for child in aexpr.args
-    if typeof(child) == AExpr && child.head == :include
-      path = child.args[1]
-      # TODO we don't want to have duplicated code
-      includedcode = preprocess(parsefromfile(path)) # Get the source code
-      append!(newargs, includedcode.args)  # we wouldn't be including a program because we have the args
-    else
-      push!(newargs, child)
-    end
-  end
-  AExpr(:program, newargs...)
-end
+#   # copy and paste the include source file into our actual autumn file
+#   newargs = []
+#   for child in aexpr.args
+#     if typeof(child) == AExpr && child.head == :include
+#       path = child.args[1]
+#       # TODO we don't want to have duplicated code
+#       includedcode = preprocess(parsefromfile(path)) # Get the source code
+#       append!(newargs, includedcode.args)  # we wouldn't be including a program because we have the args
+#     else
+#       push!(newargs, child)
+#     end
+#   end
+#   AExpr(:program, newargs...)
+# end
 # Return an AExpr where all includes have been replaced with the code that it points to
 # function sub_includes(aexpr::AExpr, already_included=Set{String}()::Set{String})
 #     # this is a short-circuit or, so then it would only compute error when the first statement is false
@@ -104,31 +104,31 @@ We name the file of the module such that the name of Module = filename
 
 Files are located in the lib folder
 """
-function sub_import(aexpr::AExpr, already_included=Set{String}()::Set{String})
-  # this is a short-circuit or, so then it would only compute error when the first statement is false
-  aexpr.head == :program || aexpr.head == :module || error("Expects program or module AExpr")
+# function sub_import(aexpr::AExpr, already_included=Set{String}()::Set{String})
+#   # this is a short-circuit or, so then it would only compute error when the first statement is false
+#   aexpr.head == :program || aexpr.head == :module || error("Expects program or module AExpr")
 
-  # copy and paste the include source file into our actual autumn file
-  newargs = []
-  for child in aexpr.args
-    if child.head == :import
-      modulename = child.args[1]
-      if modulename in already_included
-        continue
-      end
-      push!(already_included, string(modulename))
+#   # copy and paste the include source file into our actual autumn file
+#   newargs = []
+#   for child in aexpr.args
+#     if child.head == :import
+#       modulename = child.args[1]
+#       if modulename in already_included
+#         continue
+#       end
+#       push!(already_included, string(modulename))
       
-      modulepath = joinpath(AULIBPATH, string(modulename) * ".au")
-      # assume for now that modules cannot import other modules or include other code
-      includedcode = preprocess(parsefromfile(modulepath), already_included) # Get the source code
-      # includedcode = parsefromfile(modulepath)
-      append!(newargs, includedcode.args[2:end])  # don't include first arg b/c it's the module name
-    else
-      push!(newargs, child)
-    end
-  end
-  AExpr(:program, newargs...)
-end
+#       modulepath = joinpath(AULIBPATH, string(modulename) * ".au")
+#       # assume for now that modules cannot import other modules or include other code
+#       includedcode = preprocess(parsefromfile(modulepath), already_included) # Get the source code
+#       # includedcode = parsefromfile(modulepath)
+#       append!(newargs, includedcode.args[2:end])  # don't include first arg b/c it's the module name
+#     else
+#       push!(newargs, child)
+#     end
+#   end
+#   AExpr(:program, newargs...)
+# end
 
 "Produces `AExpr` from Autumn code at `path`"
 function parsefromfile(path)
