7be350b9360adff4a74e2bb4f39bfcfa306db9aa
Zenna Tavares <zennatavares@gmail.com>
2021-07-26 16:08:33 -0400

    fix assign

--- src/interpret/interpret2.jl
+++ src/interpret/interpret2.jl
@@ -37,7 +37,9 @@ function interpret(aex, Γ)
   isaexpr(x) = x isa AExpr
   MLStyle.@match arr begin
     [:program, args...]                            => (AExpr(:assign, x, interpret(v, Γ)), Γ)
-    [:assign, x, v::AExpr]                         => (AExpr(:assign, x, interpret(v, Γ)), Γ)
+    [:assign, x, v::AExpr]                         => let (v2, Γ_) = interpret(v, Γ)
+                                                        (AExpr(:assign, x, v2), Γ_)
+                                                      end
     [:assign, x, v::Symbol]                        => (AExpr(:assign, x, interpret(v, Γ)), Γ)
     [:assign, x, v]                                => (AExpr(:assign, x, v), update(Γ, x, v))
     s::Symbol                                      => (Γ[x], Γ)
@@ -46,7 +48,7 @@ function interpret(aex, Γ)
     [:if, i::AExpr, t, e]                          => (AExpr(:if, interpret(i, Γ), t, e), Γ)
     [:call, f, args...] && if !isprim(f) end       => (AExpr(:call, next(f), args...), Γ)
     [:call, f, args...] && if any(isaexpr, args) end  => (AExpr(:call, map((i, arg) -> i == shit ? next(arg) : arg)), Γ)
-    [:call, f, args...] && isprim(f) end           => (primapl(f, args...), Γ)
+    [:call, f, args...] && if isprim(f) end           => (primapl(f, args...), Γ)
     _                                              => error("Could not interpret $arr")
   end
 end
