789273f35a061674530e06ec08ccb3c2c79e6b84
Chad Scherrer <chad.scherrer@gmail.com>
2023-06-18 14:08:15 +0200

    allow Vector{Any} for constructing AExpr

--- src/base/aexpr.jl
+++ src/base/aexpr.jl
@@ -55,6 +55,7 @@ struct AExpr
   head::Symbol
   args::Vector{Any}
   AExpr(head::Symbol, @nospecialize args...) = new(head, [args...])
+  AExpr(head::Symbol, args::Vector{Any}) = new(head, args)
 end
 "Arguements of expression"
 function args end
